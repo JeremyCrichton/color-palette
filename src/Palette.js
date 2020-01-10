b7df881504ef68790f26645df59f441e83341bf4
Jeremy Crichton <you@example.com>
2020-01-10 23:01:34 +0800

    style color slider

--- src/Palette.js
+++ src/Palette.js
@@ -20,13 +20,25 @@ const Palette = props => {
   return (
     <div className="Palette">
       {/* Navbar goes here */}
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        step={100}
-        onAfterChange={handleChangeLevel}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={handleChangeLevel}
+          handleStyle={{
+            backgroundColor: 'green',
+            outline: 'none',
+            border: '2px solid green',
+            boxShadow: 'none',
+            width: '13px',
+            height: '13px',
+            marginLeft: '7px',
+            marignTop: '-3px'
+          }}
+        />
+      </div>
       <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer goes here */}
     </div>
