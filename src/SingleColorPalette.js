b45977c918f132dad67889124e7f549304831680
Jeremy Crichton <you@example.com>
2020-01-13 21:36:08 +0800

    add back link

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import seedColors from './seedColors';
 import { findPalette, generatePalette, gatherShades } from './colorHelpers';
@@ -10,8 +10,10 @@ import Footer from './Footer';
 const SingleColorPalette = () => {
   const [format, setFormat] = useState('hex');
   const { paletteId, colorId } = useParams();
+
   const palette = generatePalette(findPalette(paletteId, seedColors));
   const shades = gatherShades(palette, colorId);
+
   const colorBoxes = shades.map(color => (
     <ColorBox
       key={color.hex}
@@ -27,9 +29,16 @@ const SingleColorPalette = () => {
 
   return (
     <>
-      <div className="Palette">
+      <div className="SingleColorPalette Palette">
         <Navbar showSlider={false} changeFormat={changeFormat} />
-        <div className="Palette-colors">{colorBoxes}</div>
+        <div className="Palette-colors">
+          {colorBoxes}
+          <div className="ColorBox go-back">
+            <Link to={`/palette/${paletteId}`} className="back-button">
+              Go Back
+            </Link>
+          </div>
+        </div>
         <Footer paletteName={palette.paletteName} emoji={palette.emoji} />
       </div>
     </>
