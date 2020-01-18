c712963933e8daf932e5a12c8cde5fb9090fd796
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-01-18 14:08:49 +0800

    add background

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,19 +1,30 @@
 import { makeStyles } from '@material-ui/core/styles';
+import sizes from './sizes';
+import bg from './bg.svg';
 
 export default makeStyles({
   root: {
-    backgroundColor: 'blue',
     height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#000022',
+    backgroundImage: `url(${bg})`,
+    /* background by SVGBackgrounds.com */
+    overflow: 'scroll'
   },
   container: {
     width: '50%',
     display: 'flex',
     flexDirection: 'column',
     flexWrap: 'wrap',
-    border: '1px solid white'
+    border: '1px solid white',
+    [sizes.down('xl')]: {
+      width: '80%'
+    },
+    [sizes.down('xs')]: {
+      width: '70%'
+    }
   },
   nav: {
     color: 'white',
@@ -25,12 +36,21 @@ export default makeStyles({
       color: 'white'
     }
   },
-  title: {},
+  title: {
+    fontSize: '2rem'
+  },
   palettes: {
     boxSizing: 'border-box',
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)'
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1.4rem'
+    }
   }
 });
